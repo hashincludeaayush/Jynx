61c1ea41852ee92eeefd833f35da2df4c610baf0
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2025-02-19 13:51:31 +0530

    fixed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
